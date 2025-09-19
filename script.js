03e75f8ed6cf30933577965342365e88dd4e4a19
radityaprtama <adit.aja32636647@gmail.com>
2025-09-19 20:42:13 +0700

    Update and rename profile/script.js to script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Interaksi tombol sederhana
 document.getElementById("btnContact").addEventListener("click", function(){
   alert("Terima kasih! Tim kami akan segera menghubungi Anda.");
 });
 
-// Ambil elemen tombol berdasarkan ID
 const contactBtn = document.getElementById("contactBtn");
 
-// Tambahkan event ketika tombol diklik
 contactBtn.addEventListener("click", function() {
   alert("Terima kasih sudah ingin menghubungi kami! 😊\nSilakan DM Instagram @vibely_ecommerce_studio atau kirim email ke: hello@vibelystudio.com");
 });
@@ -14,4 +11,5 @@ contactBtn.addEventListener("click", function() {
 contactBtn.addEventListener("click", function() {
   window.open("https://www.instagram.com/vibely_ecommerce_studio", "_blank");
 });
+
 
